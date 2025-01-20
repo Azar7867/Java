30f051dea03ea4f801416e1bf87ea76e54455d13
Azar7867 <153573404+Azar7867@users.noreply.github.com>
2025-01-20 08:56:13 -0800

    Add files via upload

---
+++ OOP/StackDemo.java
@@ -0,0 +1,18 @@
+package OOP;
+
+public class StackDemo {
+	public static void main(String args[]) {
+		Stack s1=new Stack();
+		
+		s1.push(1);
+		s1.push(2);
+		s1.push(3);
+		
+		System.out.println(s1.pop());
+		
+		Stack s2=new Stack();
+		
+		
+	}
+
+}
